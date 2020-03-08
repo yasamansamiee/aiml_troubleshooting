065973b7e9fd13750cf39e7a285b47ed2f9bf8f4
Stefan <stefan.ulbrich@acceptto.com>
2020-03-08 12:59:17 +0100

    Add test files

---
+++ tests/test_em.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+r"""
+Unit tests for the algorithm.
+"""
+
+__author__ = "Stefan Ulbrich"
+__copyright__ = "Copyright 2018-2019, Acceptto Corporation, All rights reserved."
+__license__ = "Acceptto Confidential"
+__maintainer__ = "Stefan Ulbrich"
+__email__ = "Stefan.Ulbrich@acceptto.com"
+__date__ = "2020-03-08"
+
+# pylint: disable=R,C,W
+
+import pytest
+import  kuberspatiotemporal
+
+@pytest.fixture(scope="class")
+def synthetic_data():
+    return None
+
+class TestModel:
+
+    def test_batch_em(self):
+        pass
+
+    def test_incremental_em(self):
+        pass 
+
+
